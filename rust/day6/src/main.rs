e3d34bcb621f7c0c45608a6351a87b67aec89e75
Peter Fornwall <peter8a@gmail.com>
2020-12-06 08:48:03 +0100

    Added alternative iterator based solution for day6

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -31,7 +31,7 @@ fn part1(groups: &[&str]) -> usize {
     result
 }
 
-pub fn part2(groups: &[&str]) -> usize {
+fn part2(groups: &[&str]) -> usize {
     let alphabet_set: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
     fn group_intersection(s: &HashSet<char>, s2: &HashSet<char>) -> HashSet<char> {
         s.intersection(&s2).cloned().collect()
@@ -41,10 +41,53 @@ pub fn part2(groups: &[&str]) -> usize {
     result
 }
 
+// Alternative iter-based solution for part1
+fn part1_iter(groups: &[&str]) -> usize {
+    let result: usize = groups
+        .iter()
+        .map(|group| {
+            group
+                .chars()
+                .filter(|ch| ch.is_ascii_lowercase())
+                .collect::<HashSet<char>>()
+                .len()
+        })
+        .sum();
+
+    println!("Part1_iter: {}", result);
+    result
+}
+
+// Alternative iter-based solution for part2
+fn part2_iter(groups: &[&str]) -> usize {
+    let alphabet_set: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+
+    let result: usize = groups
+        .iter()
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|x| x.chars().collect())
+                .fold(alphabet_set.clone(), |acc, x| {
+                    acc.intersection(&x).cloned().collect()
+                })
+                .len()
+        })
+        .sum();
+    println!("Part2_iter: {}", result);
+    result
+}
+
 fn main() {
     let content = fs::read_to_string("../../inputs/day6_input.txt").expect("Cannot open file!");
     let v: Vec<&str> = content.trim().split("\n\n").collect();
 
     let _result_p1 = part1(&v);
     let _result_p2 = part2(&v);
+
+    let _result2_p1 = part1_iter(&v);
+    let _result2_p2 = part2_iter(&v);
+
+    assert_eq!(_result_p1, _result2_p1);
+    assert_eq!(_result_p2, _result2_p2);
 }
